ee2c372958ea8e67cb438e3afebdd3ffdaf4b4b6
Delvin <work.d6lv9n.github@gmail.com>
2025-05-09 08:45:29 -0700

    Testing real-live update on vercel

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,10 +13,8 @@ const count = ref(0)
 
   <div class="card">
     <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+
+    <p>Created by <span class="font-semibold">Christian Delvianto</span>.</p>
   </div>
 
   <p>
